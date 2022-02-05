7ec47e38d64278266c6e132ece89720a1737b7ac
Craig O'Donnell <cbodonnell@comcast.net>
2022-02-05 18:39:15 -0500

    saving scaper results into psql db

--- config.py
+++ config.py
@@ -6,8 +6,15 @@ load_dotenv() # Loads variables from .env file into environment
 
 DEBUG=True
 NAME="big.data.boys"
-REDDIT_USERNAME="cheeebz"
 VERSION="0.0.1"
+REDDIT_USERNAME="cheeebz"
 CLIENT_ID="QpA5EiFQsqYoj2nbkqrObQ"
 CLIENT_SECRET=os.environ.get("CLIENT_SECRET")
-DUMP_JSON=False
+SUBREDDIT="wallstreetbets"
+# DUMP_JSON=False
+
+DATABASE_HOST=os.environ.get("DATABASE_HOST")
+DATABASE_PORT=os.environ.get("DATABASE_PORT")
+DATABASE_NAME=os.environ.get("DATABASE_NAME")
+DATABASE_USER=os.environ.get("DATABASE_USER")
+DATABASE_PASSWORD=os.environ.get("DATABASE_PASSWORD")
--- praw-scraper.py
+++ praw-scraper.py
@@ -1,41 +1,90 @@
 import praw
-from multiprocessing import Process
+import psycopg2
 
 import config as cfg
 
 
-SUBREDDIT = "wallstreetbets"
-POST_LIMIT = 10
+# DB
+conn = psycopg2.connect(
+    database=cfg.DATABASE_NAME,
+    user=cfg.DATABASE_USER,
+    password=cfg.DATABASE_PASSWORD,
+    host=cfg.DATABASE_HOST,
+    port=cfg.DATABASE_PORT
+)
+cur = conn.cursor()
 
 
-def stream_submissions(subreddit: praw.reddit.Subreddit):
-    for submission in subreddit.stream.submissions():
-        print("--- SUBMISSION ---")
-        print(submission.title)
+def submission_exists(submission: praw.reddit.Submission) -> bool:
+    try:
+        sql = "SELECT 1 FROM submissions WHERE id = %s;"
+        cur.execute(sql, (submission.id,))
+        result = cur.fetchone()
+        exists = result is not None
+        return exists
+    except Exception as exp:
+        print(f"Error checking for submission {submission.id}: {exp}")
+        return False
 
 
-def stream_comments(subreddit: praw.reddit.Subreddit):
+def save_submission(submission: praw.reddit.Submission) -> None:
+    if cfg.DEBUG:
+        print(f"Saving submission {submission.id}...")
+        
+    try:
+        sql = """INSERT INTO submissions (id, title, author, created) 
+        VALUES (%s, %s, %s, %s);"""
+        cur.execute(sql, (
+            submission.id,
+            submission.title,
+            submission.author.name,
+            int(submission.created),
+        ))
+        conn.commit()
+    except Exception as exp:
+        conn.rollback()
+        print(f"Error saving submission {submission.id}: {exp}")
+
+
+def save_comment(comment: praw.reddit.Comment) -> None:
+    if cfg.DEBUG:
+        print(f"Saving comment on submission {comment.submission.id}...")
+    
+    try:
+        sql = """INSERT INTO comments (id, submission_id, body, author, created) 
+        VALUES (%s, %s, %s, %s, %s);"""
+        cur.execute(sql, (
+            comment.id,
+            comment.submission.id,
+            comment.body,
+            comment.author.name,
+            int(comment.created),
+        ))
+        conn.commit()
+    except Exception as exp:
+        conn.rollback()
+        print(f"Error saving comment {comment.id}: {exp}")
+
+
+def stream_comments(subreddit: praw.reddit.Subreddit) -> None:
     for comment in subreddit.stream.comments():
-        # Get comment submission
-        # Check if submission is in DB
-        # If not, save submission
+        # Check if comment submission is in DB
+        if not submission_exists(comment.submission):
+            # If not, save submission
+            save_submission(comment.submission)
         # Save comment with fk to submission
-        print(comment.body)
+        save_comment(comment)
 
 
-def main():
+def main() -> None:
     user_agent = f"script:{cfg.NAME}:{cfg.VERSION} (by /u/{cfg.REDDIT_USERNAME})" # <platform>:<app ID>:<version string> (by /u/<reddit username>)
     reddit = praw.Reddit(
         client_id=cfg.CLIENT_ID, 
         client_secret = cfg.CLIENT_SECRET,
         user_agent = user_agent,
     )
-    subreddit = reddit.subreddit(SUBREDDIT)
-    # Don't need to necessarily stream all submissions, but rather
-    # retrieve them based on the comment stream, as needed
+    subreddit = reddit.subreddit(cfg.SUBREDDIT)
     stream_comments(subreddit)
-    # Process(target=stream_submissions, args=(subreddit,)).start()
-    # Process(target=stream_comments, args=(subreddit,)).start()    
 
 
 if __name__ == "__main__":
