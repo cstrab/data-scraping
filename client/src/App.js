ea920250ea8d933e3f5b9381f151c15eaa0ad293
Craig O'Donnell <cbodonnell@comcast.net>
2022-02-08 12:42:23 -0500

    starting to dockerize

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,30 @@ import { environment } from './environment';
 
 function App() {
 
+  const DEFAULT_TIME_RANGE = 30
+  const DEFAULT_LIMIT = 10
+
+  const time_range_options = [
+    {name: "5 Minutes", value: 5},
+    {name: "15 Minutes", value: 15},
+    {name: "30 Minutes", value: 30},
+    {name: "1 Hour", value: 60},
+    {name: "12 Hours", value: 60 * 12},
+    {name: "1 Day", value: 60 * 24},
+    {name: "1 Week", value: 60 * 24 * 7},
+  ]
+
+  const limit_options = [
+    {name: "Top 5", value: 5},
+    {name: "Top 10", value: 10},
+    {name: "Top 25", value: 25},
+  ]
+
   const [mentions, setMentions] = useState([])
   const [isRefreshing, setIsRefreshing] = useState(false)
   const [refreshTime, setRefreshTime] = useState('Never')
-  const [minutes, setMinutes] = useState(30)
-  const [limit, setLimit] = useState(10)
+  const [minutes, setMinutes] = useState(DEFAULT_TIME_RANGE)
+  const [limit, setLimit] = useState(DEFAULT_LIMIT)
 
   useEffect(() => {
     const getMentions = () => {
@@ -58,71 +77,75 @@ function App() {
       {isRefreshing && <p>Refreshing...</p>}
       <div>
         <p>Time Range:</p>
-        <select onChange={onTimeRangeChanged}>
-          <option value="60">60 minutes</option>
-          <option value="30" selected>30 minutes</option>
-          <option value="15">15 minutes</option>
-          <option value="5">5 minutes</option>
+        <select defaultValue={DEFAULT_TIME_RANGE} onChange={onTimeRangeChanged}>
+          {time_range_options.map(o => <option value={o.value}>{o.name}</option>)}
         </select>
         <p>Limit:</p>
-        <select onChange={onLimitChanged}>
-          <option value="5">Top 5</option>
-          <option value="10" selected>Top 10</option>
-          <option value="25">Top 25</option>
+        <select defaultValue={DEFAULT_LIMIT} onChange={onLimitChanged}>
+          {limit_options.map(o => <option value={o.value}>{o.name}</option>)}
         </select>
       </div>
       
-      <Plot
-          data={[
-            {
-              type: 'bar',
-              x: mentions.map(m => m.symbol),
-              y: mentions.map(m => m.count),
-              marker: {
-                color: mentions.map(m => {
-                  return `RGB(${scaleValue(m.sentiment, 0, 255, 1, -1)}, ${scaleValue(m.sentiment, 0, 255, -1, 1)}, 0 )`
-                }),
-                // size: mentions.map(m => scaleValue(m.count, 1, 2, -1, 1)),
-              }
-            },
-          ]}
-          layout={{
-            title: `WSB Top ${limit}`,
-            xaxis: { 
-              title: 'Symbol'
-            },
-            yaxis: { 
-              title: 'Mentions'
-            },
-          }}
-      />
-      <Plot
-          data={[
-            {
-              type: 'scatter',
-              mode: 'markers',
-              x: mentions.map(m => m.sentiment),
-              y: mentions.map(m => m.count),
-              hovertext: mentions.map(m => m.symbol),
-              marker: {
-                color: mentions.map(m => {
-                  return `RGB(${scaleValue(m.sentiment, 0, 255, 1, -1)}, ${scaleValue(m.sentiment, 0, 255, -1, 1)}, 0 )`
-                }),
-                // size: mentions.map(m => scaleValue(m.count, 1, 2, -1, 1)),
-              }
-            },
-          ]}
-          layout={{
-            title: `WSB Top ${limit}`,
-            xaxis: { 
-              title: 'Sentiment',
-              range: [-1, 1] 
-            },
-            yaxis: { 
-              title: 'Mentions'
-            },
-          }}
-      />
+      <div>
+        <Plot
+            data={[
+              {
+                type: 'bar',
+                x: mentions.map(m => m.symbol),
+                y: mentions.map(m => m.count),
+                marker: {
+                  color: mentions.map(m => {
+                    return `RGB(${scaleValue(m.sentiment, 0, 255, 1, -1)}, 0, ${scaleValue(m.sentiment, 0, 255, -1, 1)})`
+                  }),
+                  // size: mentions.map(m => scaleValue(m.count, 1, 2, -1, 1)),
+                }
+              },
+            ]}
+            layout={{
+              title: `WSB Top ${limit}`,
+              xaxis: { 
+                title: 'Symbol'
+              },
+              yaxis: { 
+                title: 'Mentions'
+              },
+            }}
+            // config={{
+            //   responsive: true
+            // }}
+            useResizeHandler = {true}
+            style = {{width: "100%", height: "100%"}}
+        />
+        <Plot
+            data={[
+              {
+                type: 'scatter',
+                mode: 'markers',
+                x: mentions.map(m => m.sentiment),
+                y: mentions.map(m => m.count),
+                hovertext: mentions.map(m => m.symbol),
+                marker: {
+                  color: mentions.map(m => {
+                    return `RGB(${scaleValue(m.sentiment, 0, 255, 1, -1)}, 0, ${scaleValue(m.sentiment, 0, 255, -1, 1)})`
+                  }),
+                  // size: mentions.map(m => scaleValue(m.count, 1, 2, -1, 1)),
+                }
+              },
+            ]}
+            layout={{
+              title: `WSB Top ${limit}`,
+              xaxis: { 
+                title: 'Sentiment',
+                range: [-1, 1] 
+              },
+              yaxis: { 
+                title: 'Mentions'
+              },
+            }}
+            useResizeHandler = {true}
+            style = {{width: "100%", height: "100%"}}
+        />
+      </div>
     </div>
   );
 }
